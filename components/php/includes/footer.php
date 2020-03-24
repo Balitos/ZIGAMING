c69e63ea059a5c2d2d572f7c9f8a0feb4fdcb8f5
Bk201li <56292702+Bk201li@users.noreply.github.com>
2020-03-24 20:58:35 +0100

    Footer

--- components/php/includes/footer.php
+++ components/php/includes/footer.php
@@ -1,6 +1,11 @@
 <!-- FOOTER -->
 <footer>
-    
+<div id="footer-text">
+        <p>© 2020 zigaming.com.<span>•</span>Tous Droits Réservés</p>
+    </div>
+    <div id="footer-text">
+        <p>This site is protected by reCAPTCHA and the Google Privacy Policy, Terms of Service and YouTube's Terms of Service apply.</p>
+    </div>
 </footer>
 <!-- FONT AWESOME KIT -->
 <script src="https://kit.fontawesome.com/e6c2645393.js" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -13,6 +13,9 @@
     include "components/php/includes/header.php";
     ?>
     <!-- WRAPPER -->
+    <div id="wrapper">
+
+    </div>
     <!-- FOOTER -->
     <?php
     include "components/php/includes/footer.php";
