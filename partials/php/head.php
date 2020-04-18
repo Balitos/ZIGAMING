0697ad4d6848cd0201d7895fbfb5de524e0ce61d
Nicolas <nicolas.lafond@ynov.com>
2020-04-18 18:42:10 +0200

    SEO

--- partials/php/head.php
+++ partials/php/head.php
@@ -2,5 +2,6 @@
 <link rel="stylesheet" href="style.css">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta name="description" content="Avec zigaming, trouvez des jeu ps4, xbox, pc a pris réduis sur zigaming">
+<meta name="keywords" content="zigaming, jeu,pas chère" />
 <title>ZIGAMING</title>
