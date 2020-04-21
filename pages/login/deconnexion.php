752e7b3f2d48d0cfdfa9ee4eb44a72d9390c64e5
Bk201li <56292702+Bk201li@users.noreply.github.com>
2020-04-21 11:43:47 +0200

    clear

--- pages/login/deconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-setcookie('email', '', time()-3600, "index.php");
-setcookie('password', '', time()-3600, "index.php");
-$_SESSION = array();
-session_destroy();
-header("Location: ../login/");
-?>
